ca956e41d2895fc947e89d2f620293d2bfa380d7
KitEA <kit.earf@gmail.com>
2024-05-20 17:50:37 +0400

    crawl single page  with error handling

--- crawl.js
+++ crawl.js
@@ -42,7 +42,30 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return urls;
 }
 
+async function crawlPage(url) {
+    try {
+        const response = await fetch(url);
+
+        if (response.status > 399) {
+            console.log(`error in fetch with status code: ${response.status}, on page: ${url}`)
+            return
+        } 
+        
+        const contentType = response.headers.get('content-type');
+        
+        if (!contentType.includes('text/html')) {
+            console.log(`not html response, content type: ${contentType}, on page: ${url}`)
+            return
+        }
+
+        console.log(await response.text());
+    } catch (error) {
+        console.log(`error in fetch: ${error.message}, on page: ${url}`)
+    }
+}
+
 module.exports  = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const { crawlPage } = require('./crawl')
+
+function main() {
+    if (process.argv.length > 3 || process.argv.length < 3) {
+        console.log('Programm accepts only one website!');
+        process.exit(1);
+    } else {
+        console.log(`Starting a crawling process: ${process.argv[2]}`);
+    }
+    
+    const baseURL = process.argv[2];
+    crawlPage(baseURL);
+}
+
+main();
