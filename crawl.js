9d52d8e11926c64a1a392b91c98808fe083e7df6
KitEA <kit.earf@gmail.com>
2024-05-27 22:52:44 +0400

    crawl full site

--- crawl.js
+++ crawl.js
@@ -1,5 +1,71 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+    if (baseURL !== currentURL && notOnTheSameDomain(currentURL, baseURL)) {
+        return pages;
+    }
+    const normalizedCurrentURL = normalizeURL(currentURL);
+
+    if (pages[normalizedCurrentURL]) {
+        pages[normalizedCurrentURL]++;
+        return pages;
+    }
+    pages[normalizedCurrentURL] = 1;
+
+    console.log(`actively crawling: ${currentURL}`)
+
+    let html = '';
+    try {
+        html = await fetchHTML(currentURL); 
+    } catch (error) {
+        console.log(`${error.message}`)
+        return pages;
+    }
+    pages = await parseTheHTML(html, baseURL, pages);
+    
+    return pages;
+}
+
+async function fetchHTML(url) {
+    let response
+    try {
+      response = await fetch(url, {
+        method: "GET",
+        mode: "cors"
+      });
+    } catch (err) {
+      throw new Error(`Network error: ${err.message}`);
+    }
+  
+    if (response.status > 399) {
+      throw new Error(`HTTP error: ${response.status} ${response.statusText}`);
+    }
+  
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('text/html')) {
+      throw new Error(`Non-HTML response: ${contentType}`);
+    }
+  
+    return response.text();
+  }
+
+  async function parseTheHTML(html, baseURL, pages) {;
+    const urls = getURLsFromHTML(html, baseURL);
+
+    for (const url of urls) {
+        pages = crawlPage(baseURL, url, pages);
+    }
+
+    return pages;
+}
+
+function notOnTheSameDomain(currentURL, baseURL) {
+    const baseUrlObj = new URL(baseURL);
+    const currentUrlObj = new URL(currentURL);
+    
+    return baseUrlObj.hostname !== currentUrlObj.hostname;
+}
+
 function normalizeURL(url) {
     if (url.length === 0) {
         return url;
@@ -55,23 +121,6 @@ function addAbsouluteURL(linkElement, urls) {
         urls.push(urlObj.href);
     } catch (error) {
         console.log(`Invalid URL: ${error.message}`);
-    }
-}
-
-async function crawlPage(url) {
-    try {
-        const response = await fetch(url);
-        const contentType = response.headers.get('content-type');
-
-        if (response.status > 399) {
-            console.log(`error in fetch with status code: ${response.status}, on page: ${url}`);
-        } else if (!contentType.includes('text/html')) {
-            console.log(`not html response, content type: ${contentType}, on page: ${url}`);
-        } else {
-            console.log(await response.text());
-        }
-    } catch (error) {
-        console.log(`error in fetch: ${error.message}, on page: ${url}`);
     }
 }
 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require('./crawl')
 
-function main() {
+async function main() {
     if (process.argv.length > 3 || process.argv.length < 3) {
         console.log('Programm accepts only one website!');
         process.exit(1);
@@ -9,7 +9,11 @@ function main() {
     }
     
     const baseURL = process.argv[2];
-    crawlPage(baseURL);
+    const pages = await crawlPage(baseURL);
+
+    for (const page of Object.entries(pages)) {
+        console.log(page);
+    }
 }
 
 main();
